79dcc7c71f811a0e478c90ee747b5be1b7cff70c
Kenn1412 <baohungdinhnguyen1412@gmail.com>
2024-03-04 00:45:09 +0700

    Exercise 12 - Mongoose Part 3

--- Exercise 11/node-mongoose/index.js
+++ Exercise 11/node-mongoose/index.js
@@ -1,39 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const Dishes = require("./models/dishes");
 
-const Dishes = require('./models/dishes');
-
-
-const url = 'mongodb://localhost:27017/conFusion2';
+const url = "mongodb://localhost:27017/conFusion2";
 const connect = mongoose.connect(url);
 
-
 connect.then((db) => {
+  console.log("Connected correctly to server");
 
+  Dishes.create({
+    name: "Uthappizza",
+    description: "Original",
+  })
+    .then((dish) => {
+      console.log(dish);
 
-    console.log('Connected correctly to server');
-
-
-    Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        {
+          $set: { description: "Updated test" },
+        },
+        {
+          new: true,
+        }
+      ).exec();
     })
     .then((dish) => {
-        console.log(dish);
-        
-        return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-        console.log(dishes);
+      console.log(dish);
 
+      dish.comments.push({
+        rating: 5,
+        comment: "Mon nay cho 5 sao",
+        author: "Ken khong ngu",
+      });
 
-        return Dishes.deleteOne({});
+      return dish.save();
     })
+    // .then((dish) => {
+    //   console.log(dish);
+
+    //   return Dishes.deleteOne({});
+    // })
     .then(() => {
-        return mongoose.connection.close();
+      return mongoose.connection.close();
     })
     .catch((err) => {
-        console.log(err);
+      console.log(err);
     });
-
 });
